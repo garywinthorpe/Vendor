ff298626c12acbf071285782e12f1b0c55ffa200
garywinthorpe <hunterbrownell@gmail.com>
2017-11-09 11:42:03 -0800

    Done!

---
+++ Vendor.java
@@ -0,0 +1,97 @@
+/**
+ * This class implements a vendor that sells one kind
+ * of items. A vendor carries out sales transactions.
+ */
+public class Vendor
+{
+  private int price
+  private int stock
+  private int deposit
+  private int price;
+
+  /**
+   * Constructs a Vendor
+   * @param price the price of a single item in cents (int)
+   * @param stock number of items to place in stock (int)
+   */
+  public Vendor(int p, int s)
+  {
+    price = p;
+	stock = s;
+  }
+
+  /**
+   * Sets the quantity of items in stock.
+   * @param qty number of items to place in stock (int)
+   */
+  public void setStock(int n)
+  {
+    stock = n;
+  }
+
+  /**
+   * Returns the number of items currently in stock.
+   * @return number of items currently in stock (int)
+   */
+  public int getStock()
+  {
+    return stock;
+  }
+
+  /**
+   * Adds a specified amount (in cents) to the
+   * deposited amount.
+   * @param number of cents to add to the deposit (int)
+   */
+  public void addMoney(int d) 
+  {
+    deposit = d + deposit;
+  }
+
+  /**
+   * Returns the currently deposited amount (in cents).
+   * @return number of cents in the current deposit (int)
+   */
+  public int getDeposit()
+  {
+    return deposit;
+  }
+
+  /**
+   * Implements a sale.  If there are items in stock and
+   * the deposited amount is greater than or equal to
+   * the single item price, then adjusts the stock
+   * and calculates and sets change and returns true;
+   * otherwise refunds the whole deposit (moves it into
+   * change) and returns false.
+   * @return true for a successful sale, false otherwise (boolean)
+   */
+  public boolean makeSale()
+  {
+    if(stock>0 && deposit >= price)
+	{
+		stock--;
+		returnedAmount=deposit - price;
+		deposit = 0;
+		return true;
+	}
+	else
+	{
+		returnedAmount = deposit;
+		deposit = 0;
+		return false;
+	}
+  }
+
+  /**
+   * Returns and zeroes out the amount of change (from
+   * the last sale or refund).
+   * @return number of cents in the current change (int)
+   */
+  public int getChange()
+  {
+    int r = returnedAmount;
+	returnedAmount = 0;
+	return r;
+  }
+}
